d56caa4188492b1f3572e0cb18d60a334c8c5c83
Érico Soares <ericoms1996@gmail.com>
2018-02-04 21:38:31 -0200

    basic express and socket structure done

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var socket = require('socket.io');
+var app = express();
+
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render(__dirname + '/public/views/home');
+});
+
+var server = app.listen(process.env.PORT || 3000, () => {
+	console.log("Listening to port 3000");
+});
+
+var io = socket(server);
+
+io.on('connection', (socket) => {
+	console.log('made socket connection');
+});
---
+++ public/js/app.js
@@ -0,0 +1,4 @@
+$(document).ready(() => {
+	alert("asdas");
+	var socket = io.connect('http://localhost:3000');
+});
