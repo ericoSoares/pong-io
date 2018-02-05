feca34b4251078ee5f5d4e43182c861e83a5dfb1
Érico Soares <ericoms1996@gmail.com>
2018-02-05 15:32:28 -0200

    basic ball and player rendering

--- index.js
+++ index.js
@@ -15,12 +15,49 @@ var server = app.listen(process.env.PORT || 3000, () => {
 });
 
 var io = socket(server);
-
+var players = [];
+var ball = {
+	xPos: 250,
+	yPos: 250,
+	speed: 1,
+	color: "red"
+};
 io.on('connection', (socket) => {
 	console.log('Made socket connection' + socket.id);
+	var player = {
+		id: socket.id,
+		xPos: 0,
+		yPos: 0,
+		team: "right"
+	};
+	players.push(player);
 
 	socket.on('action', (data) => {
 		console.log(data);
-		io.sockets.emit('action', data);
+		for(var i = 0; i < players.length; i++) {
+			if(players[i].id == player.id) {
+				if(players[i].team == "left") {
+					players[i].xPos = (data.xPos > 250)?250:data.xPos;
+					players[i].yPos = data.yPos;
+				} else {
+					players[i].xPos = (data.xPos < 250)?250:data.xPos;
+					players[i].yPos = data.yPos;
+				}
+			}
+		}
+		io.sockets.emit('update', {players: players, ball: ball});
 	});
+
+	setInterval(() => {
+		if(ball.xPos < 500 && ball.yPos < 500) {
+			ball.xPos++;
+			ball.yPos++;
+			ball.speed -= 0.1;
+		} else {
+			ball.xPos = 250;
+			ball.yPos = 250;
+			ball.speed = 1;
+		}
+		io.sockets.emit('update', {players: players, ball: ball});
+	}, 1000/60);
 });
--- public/js/app.js
+++ public/js/app.js
@@ -2,15 +2,32 @@ var socket = io.connect('http://localhost:3000');
 var canvas = document.getElementById('board');
 var ctx    = canvas.getContext('2d');
 
-$("#board").click((ev) => {
+$("#board").mousemove((ev) => {
 	socket.emit('action', {
 		xPos: ev.pageX - $('#board').offset().left,
 		yPos: ev.pageY - $('#board').offset().top
 	});
 });
 
-socket.on('action', (data) => {
-	ctx.beginPath();
-	ctx.arc(data.xPos,data.yPos,10,0,2*Math.PI);
-	ctx.fill();
+socket.on('update', (data) => {
+	updateCanvas(data);
 });
+
+socket.on('ballmove', (data) => {
+	updateCanvas(data);
+});
+
+function updateCanvas(data) {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	for(var i = 0; i < data.players.length; i++) {
+		ctx.beginPath();
+		ctx.arc(data.players[i].xPos,data.players[i].yPos,10,0,2*Math.PI)
+		ctx.fillStyle = "black";
+		ctx.fill();
+	}
+	ctx.beginPath();
+	ctx.arc(data.ball.xPos,data.ball.yPos,10,0,2*Math.PI);
+	ctx.fillStyle = data.ball.color;
+	ctx.fill();
+}
+	
