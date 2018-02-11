be202da3f741070c859d09251af04fb36dae919a
Érico Soares <ericoms1996@gmail.com>
2018-02-11 21:46:09 -0200

    settings working

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,51 @@
-var settings = {
-	username: "Fodase",
-	team: "left",
-	theme: "dark",
-	screenSize: 3
-};
+var slide = false;
+var size = 2;
+var name = "";
 
 $("input[name=teamButton]").click(() => {
-	var aux = $("input[name=teamButton]");
-	if(aux.val() == "Right")
-    	aux.val("Left");
-  	else
-    	aux.val("Right");
-    $("input[name=team]").val(aux.val());
+  if($("input[name=teamButton]").val() == "Right")
+    $("input[name=teamButton]").val("Left");
+  else
+    $("input[name=teamButton]").val("Right");
+  $("input[name=team]").val($("input[name=teamButton]").val().toLowerCase());
 });
+
+$("input[name=settings]").click(() => {
+  if(!slide) {
+    $("#settingsList").slideDown(300);
+    $("input[name=settings]").addClass("hovered");
+    slide = true;
+  } else {
+    $("#settingsList").slideUp(300);
+    $("input[name=settings]").removeClass("hovered");
+    slide = false;
+  }
+}); 
+
+$("#theme").click(() => {
+  var theme = $("#theme");
+  if(theme.text() == "Theme: Dark")
+    theme.text("Theme: Light");
+  else 
+    theme.text("Theme: Dark");
+  $("input[name=theme]").val(theme.text().includes("Dark") ? "dark": "light");
+});
+
+$("#displayNames").click(() => {
+  var el = $("#displayNames");
+  if($("i").length == 0) {
+    el.append('<i class="fa fa-check" aria-hidden="true"></i>');
+    $("input[name=displayNames]").val("false");
+  } else {
+    el.children("i").remove();
+    $("input[name=displayNames]").val("true");
+  }
+});
+
+$("#screenSize").click(() => {
+  var sizes = ['small', 'medium', 'large'];
+  size = (size == 2) ? 0 : size+1;
+  $("input[name=screenSize]").val(sizes[size]);
+  $("#screenSize").text("Screen size: " + sizes[size][0].toUpperCase() + sizes[size].substring(1, sizes[size].length));
+});
+ 
